390b45fc83138589cc940d70088d94d139b6d12b
Justin Boyan <jboyan@gmail.com>
2016-01-06 13:58:22 -0500

    Initial Commit

---
+++ Test Number 1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test Number 1
+//
+//  Created by JA Boyan on 1/6/16.
+//  Copyright © 2016 JA Boyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test Number 1/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Test Number 1
+//
+
+//  Copyright (c) 2016 JA Boyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
